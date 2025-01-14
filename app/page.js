3326634e672dfc3e0709eba3df63aadbc9140b27
Jan Dahlke <jan.dahlke@akquinet.de>
2025-01-14 15:49:50 +0100

    more dummy content for frontpage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,52 @@
-import Link from "next/link";
+import Link from 'next/link';
+
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.slideshow}>
+          
+        </div>
+        <div>
+          <div className={styles.hero}>
+            <h1>NextLevel food for akquinet foodies</h1>
+            <p>Taste and share food from all over the world or other fancy marketing text</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href="/community">Join the community</Link>
+            <Link href="/meals">Explore meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={styles.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+### learning next
+
+* is kicked off with `npx create-next-app`
+* run `npm install` followed by `npm run dev`
