6891549ce2d13c7354d98f4bfbfdff95a50bfc6c
Jan Dahlke <jan.dahlke@akquinet.de>
2025-01-15 11:40:11 +0100

    populate meals details page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,40 @@
-const MealDetailsPage = () => {
+import Image from 'next/image';
+
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+const MealDetailsPage = ({ params }) => {
+
+  const {
+    title,
+    image,
+    summary,
+    instructions,
+    creator,
+    creator_email,
+  } = getMeal(params.slug) // the params obj will have a [slug] param, see dynamic route creation
+
   return (
-    <h1>MealDetailsPage</h1>
+    <>
+      <header className={ styles.header}>
+        <div className={styles.image}>
+          <Image src={image}  fill/>
+        </div>
+        <div className={styles.headerText}>
+          <h1>{ title }</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${creator_email}`}>{ creator}</a>
+          </p>
+          <p className={summary}>
+            summary
+          </p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: instructions.replace(/\n/g, '<br />')}}></p>
+      </main>
+    </>
   )
- };
+};
 
 export default MealDetailsPage;
