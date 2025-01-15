ec48f5e473839a46da3ce1b73d926565e43c9af8
Jan Dahlke <jan.dahlke@akquinet.de>
2025-01-15 11:03:50 +0100

    404 not found example

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>The page you requested is not found</h1>
+      <p>go back!</p>
+    </main>
+  )
+}
+
+export default NotFoundPage;
